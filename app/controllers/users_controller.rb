9de6a1342318eb4a2752ffd39ce1b4ea2b7a5171
RussellAbraham <russell_a@live.com>
2023-11-08 10:19:05 -0800

    Add email validation and bootstrap classes for the forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      redirect_to signup_path
+      redirect_to signup_path(error: user.errors.full_messages.first)
     end
   end
 
